f84379d7862f5bb3ab58a08515517e6b5b1ae873
Pieter-degeneffe <pieterdegeneffe@outlook.com>
2019-11-20 13:40:08 +0100

    Added auth0 authentication

--- app.js
+++ app.js
@@ -1,30 +1,51 @@
+'use strict';
+
 // import dependencies
 const express = require('express');
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
 const bodyParser = require('body-parser');
 const cors = require('cors');
 //const morgan = require('morgan');
 const mongoose = require('mongoose');
-const basicAuth = require('express-basic-auth')
 require('dotenv').config();
+
+// Set up Auth0 configuration
+const authConfig = require("./auth_config.json");
+if (!authConfig.domain || !authConfig.audience) {
+  throw "Please make sure that auth_config.json is in place and populated";
+}
 
  // create the express app
 const PORT = process.env.PORT || 8081; // client is running on 8080
-const app = express()
+const app = express();
 
 // make the app use dependencies
 //app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use(basicAuth({
-    users: { 'admin': process.env.BASIC_AUTH_PASSWORD }
-}))
+// Middleware that validates incoming bearer tokens using JWKS from equibase.eu.auth0.com
+const authCheck = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ["RS256"]
+});
 
 //Connect to database
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
 mongoose.connect(process.env.MONGODB_URI);
+
+app.use('/', authCheck);
 
 //Mounting of routes
 const horse = require('./routes/api/horse');
