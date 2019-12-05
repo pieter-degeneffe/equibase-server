03a4a57082efb1992011fd1c23adf4fce5cb1d6c
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2019-12-05 15:37:45 +0100

    Added contact schema to customer

--- models/customer.js
+++ models/customer.js
@@ -1,6 +1,47 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+let contactSchema = new Schema({
+  first_name: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    required: [true, 'First name of contact is required'],
+    maxlength: [64, 'Max length is 64 characters'],
+    match: [/^[a-zA-Z0-9]+$/, 'Please fill a valid first name'],
+    index: true
+  },
+  last_name: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    required: true,
+    required: [true, 'Last name of contact is required'],
+    maxlength: [64, 'Max length is 64 characters'],
+    match: [/^[a-zA-Z0-9]+$/, 'Please fill a valid last name'],
+    index: true
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    maxlength: [64, 'Max length is 64 characters'],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    index: true
+  },
+  telephone: {
+    type: String,
+    trim: true,
+    maxlength: [16, 'Max length is 16 characters'],
+    lowercase: true
+  },
+  role: {
+    type: String,
+    enum: ['vos','zwart','bruin']
+  }
+});
+
 let customerSchema = new Schema({
   type: {
     type: String,
@@ -93,43 +134,7 @@ let customerSchema = new Schema({
     required: [true, 'Country is required'],
     enum: ['Afghanistan','Albanië','Algerije','Andorra','Angola','Antigua-Barbuda','Argentinië','Armenië','Aruba','Australië','Azerbaijan','Bahamas','Bahrein','Belize','België','Bermuda','Bolivia','Bosnië-Herzegovina','Botswana','Brazilië','Brunei Darussalam','Bulgarije','Burundi','Cambodja','Cameroen','Canada','Cayman Eilanden','Centraal-Afrikaanse Republiek','Chili','China','Ciprus','Colombia','Congo','Cook Eilanden','Costa Rica','Groatië','Cuba','Cyprus','Denemarken','Dominica','Dominicaanse Republiek','DR Congo','Duitsland','Ecuador','Egypte','El Salvador','Eritrea','Estland','Ethiopië','Fiji','Filipijnen','Finland','Frankrijk','Frans Polynesië','Gabon','Gambia','Georgië','Ghana','Griekenland','Groenland','Guam','Guatemala','Guinee-Bissau','Guyana','Haïti','Honduras','Hongarije','Ierland','IJsland','India','Indonesië','Irak','Iran','Israël','Italië','Ivoorkust','Jamaica','Japan','Jemen','Joegoslavië','Jordanië','Kameroen','Kazachstan','Kenya','Kirgizstan','Koeweit','Korea','Kroatië','Laos','Lesotho','Letland','Libanon','Liberia','Libië','Liechtenstein','Litouwen','Luxemburg','Macedonië','Maleisië','Mali','Malta','Marokko','Mauritanië','Mauritius','Mexico','Moldova','Monaco','Mozambique','Namibië','Nederland','Nepal','Nicaragua','Nieuw Zeeland','Niger','Nigeria','Noorwegen','Oezbekistan','Oman','Oostenrijk','Pakistan','Papoea-Nieuw-Guinea','Paraguay','Peru','Polen','Portugal','Puerto Rico','Quatar','Roemenië','Rusland','Rwanda','Saint Lucia','Salomonseilanden','San Marino','Saudi-Arabië','Schotland','Senegal','Sierra Leone','Singapore','Slovenië','Slowakije','Somalië','Spanje','Sri Lanka','Sudan','Syrie','Tadzjikistan','Taiwan','Thailand','Tobago','Tsjechië','Tsjaad','Tunesië','Turkije','Turkmenistan','Trinidad','Uganda','Ukraine','Uruguay','Venezuela','Verenigd Koninkrijk','Verenigde Staten','Vietnam','Zaïre','Zambia','Zimbabwe','Zuid-Afrika','Zweden','Zwitserland']
   },
-  contacts : [{
-    first_name: {
-      type: String,
-      trim: true,
-      lowercase: true,
-      maxlength: [64, 'Max length is 64 characters'],
-      match: [/^[a-zA-Z0-9]+$/, 'Please fill a valid first name'],
-      index: true
-    },
-    last_name: {
-      type: String,
-      trim: true,
-      lowercase: true,
-      maxlength: [64, 'Max length is 64 characters'],
-      match: [/^[a-zA-Z0-9]+$/, 'Please fill a valid last name'],
-      index: true
-    },
-    email: {
-      type: String,
-      trim: true,
-      lowercase: true,
-      unique: true,
-      maxlength: [64, 'Max length is 64 characters'],
-      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
-      index: true
-    },
-    telephone: {
-      type: String,
-      trim: true,
-      maxlength: [16, 'Max length is 16 characters'],
-      lowercase: true
-    },
-    role: {
-      type: String,
-      enum: ['vos','zwart','bruin']
-    }
-  }]
+  contacts : [contactSchema]
 }, {timestamps: true});
 
 module.exports = mongoose.model('Customer', customerSchema);
