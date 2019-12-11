1452c873f101290e6d2d003e5fefffb3a9224e1d
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2019-12-11 15:09:51 +0100

    Updated horse fields

--- models/horse.js
+++ models/horse.js
@@ -17,13 +17,20 @@ let horseSchema = new Schema({
     trim: true,
     maxlength: [15, 'Max length is 15 characters']
   },
+  microship: {
+    type: String
+  },
   date_of_birth: {
     type: Date,
     max: [Date.now, "Date of birth can't be in the future"]
   },
-  studbook_number: {
+  studbook: {
     type: String,
-    trim: true
+    enum: ['Aes','American Quarter Horse','Anglo-arabian','Arab','Bwp','Hannover','Holstein','Kwpn','Lusitana','Oldenburg','Others','Sbs','Sf','Stud-book Du Cheval De Selle Luxembourgeois (S.c.s.l.)','Westfalen','Zangersheide']
+  },
+  coat_color: {
+    type: String,
+    enum: ['vos','zwart','bruin']
   },
   father: {
     type: String,
@@ -40,15 +47,22 @@ let horseSchema = new Schema({
     trim: true,
     maxlength: [64, 'Max length is 64 characters']
   },
-  coat_color: {
-    type: String,
-    enum: ['vos','zwart','bruin']
+  passport: {
+    type: String
+  },
+  status: {
+    type: Boolean
   },
   surrogate: {
     type: Boolean
   },
-  passport: {
-    type: String
+  surrogate_uid: {
+    type: String,
+    maxlength: [64, 'Max length is 64 characters']
+  },
+  surrogate_location: {
+    type: String,
+    enum: ['locatie 1','locatie 2','locatie 3']
   },
   owner : {
     type: mongoose.Schema.Types.ObjectId,
