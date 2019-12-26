eddaa3b4251176cd6d649b6075142cb711455d87
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2019-12-26 13:45:59 +0100

    Add location controller

--- routes/api/horse.js
+++ routes/api/horse.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const horseController = require('../../controllers/horse.js');
+const locationController = require('../../controllers/location.js');
 
 router.get('/', horseController.getAllHorses);
 router.get('/count', horseController.getHorseCount);
 router.get('/:id', horseController.getHorse);
-router.post('/', horseController.createHorse);
-router.put('/:id', horseController.updateHorse);
+router.post('/', locationController.checkAvailablePlaces, horseController.createHorse);
+router.put('/:id', locationController.checkAvailablePlaces, horseController.updateHorse);
 router.delete('/:id', horseController.deleteHorse);
 router.post('/:id/passport', horseController.createPassport);
 router.delete('/:id/passport', horseController.deletePassport);
