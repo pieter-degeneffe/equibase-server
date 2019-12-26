06d97f849896ce578add93c471e2f5be336c2482
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2019-12-26 13:45:14 +0100

    Add location places

--- models/location.js
+++ models/location.js
@@ -7,6 +7,10 @@ let locationSchema = new Schema({
     trim: true,
     required: [true, 'Location name is a required field'],
     maxlength: [64, 'Max length is 64 characters']
+  },
+  places: {
+    type: Number,
+    required: [true, 'Location places is a required field'],
   }
 }, {timestamps: true});
 
