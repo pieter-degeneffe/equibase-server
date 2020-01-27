2ee273e6f0adfd6708ab0ba0aade1513e5c3687c
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2020-01-27 12:40:51 +0100

    Added Nitrogen Container controller

---
+++ controllers/nitrogenContainer.js
@@ -0,0 +1,52 @@
+const NitrogenContainer = require('../models/nitrogenContainer.js');
+
+//Create a new nitrogen container
+exports.createNitrogenContainer = async (req, res, next) => {
+  try {
+    let nitrogenContainer = new NitrogenContainer(req.body.nitrogenContainer);
+    await nitrogenContainer.save((err) => {
+      if (err) return next(err);
+      res.status(201).send(nitrogenContainer);
+    })
+  } catch (err) {
+    return next(err);
+  }
+};
+
+//Get all nitrogen containers
+exports.getNitrogenContainers = async (req,res,next) => {
+  try {
+    await NitrogenContainer.find(req.query)
+      .exec((err, nitrogenContainers) => {
+        if (err) return next(err);
+        res.status(200).send(nitrogenContainers);
+      });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+//Update a nitrogen container
+exports.updateNitrogenContainer = async (req, res, next) => {
+  try {
+    const response = await NitrogenContainer.findByIdAndUpdate(req.params.id, {$set: req.body.nitrogenContainer}, { new: true }, (err, nitrogenContainer) => {
+      if (err) return next(err);
+      res.status(200).send(nitrogenContainer);
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+//Delete a nitrogen container
+exports.deleteNitrogenContainer = async (req,res,next) => {
+  try {
+    NitrogenContainer.findByIdAndDelete(req.params.id, (err, nitrogenContainer) => {
+      if (err) return next(err);
+      res.status(200).send(`The customer was succesfully deleted`);
+    });
+  }
+  catch(err) {
+    return res.status(500).send(err);
+  }
+};
