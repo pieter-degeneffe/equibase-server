2b2d324e867445379c57b42d570bc3c1c0ea5eee
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2020-01-27 12:43:01 +0100

    Disabled error handler

--- helpers/error.js
+++ helpers/error.js
@@ -1,21 +1,22 @@
-class ErrorHandler extends Error {
-  constructor(statusCode, message) {
-    super();
-    this.statusCode = statusCode;
-    this.message = message;
-  }
-};
-const handleError = (err, res) => {
-  if(!err.statusCode) err.statusCode = 500;
-  const { statusCode, message } = err;
-  res.status(statusCode).json({
-    status: "error",
-    statusCode,
-    message
-  });
-  // res.status(500).send('Something broke!')
-};
-module.exports = {
-  ErrorHandler,
-  handleError
-}
+// class ErrorHandler extends Error {
+//   constructor(statusCode, message) {
+//     super();
+//     this.statusCode = statusCode;
+//     this.message = message;
+//   }
+// };
+// const handleError = (err, res) => {
+//   console.log(err);
+//   // if(!err.statusCode) err.statusCode = 500;
+//   if(!err.message) err.message = err.errmsg;
+//   const { statusCode, message } = err;
+//   res.status(statusCode).json({
+//     status: "error",
+//     statusCode,
+//     message
+//   });
+// };
+// module.exports = {
+//   ErrorHandler,
+//   handleError
+// }
