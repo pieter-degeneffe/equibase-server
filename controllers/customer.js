36e6cdedbbc15a1fbc884fd7849897f7f60352b2
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2020-02-19 09:11:38 +0100

    Code cleanup

--- controllers/customer.js
+++ controllers/customer.js
@@ -26,32 +26,10 @@ exports.getAllCustomers = async (req, res, next) => {
   }
 };
 
-//Search customers
-exports.getCustomerSearch = async (req, res, next) => {
-  try {
-    const respons = await Customer.fuzzySearch(req.params.id);
-    res.status(201).send(respons);
-  } catch(err) {
-    return next(err);
-  }
-};
-
-//Get customer count
-exports.getCustomerCount = async (req, res, next) => {
-  try {
-    await Customer.countDocuments({}, (err, count) => {
-      if (err) res.status(404).send();
-      res.status(201).json(count);
-    });
-  } catch(err) {
-    return next(err);
-  }
-};
-
-//Get a specific customer
+//Get a customer
 exports.getCustomer = async (req,res,next) => {
   try {
-    await Customer.findById(req.params.id, (err, customer) => {
+    await Customer.findById(req.params.customerId, (err, customer) => {
       if (err) res.status(404).send();
       res.status(200).send(customer);
     });
@@ -60,48 +38,22 @@ exports.getCustomer = async (req,res,next) => {
   }
 };
 
-// Get the horses of a specific customer
-exports.getHorsesOfCustomer = async (req,res,next) => {
-  try {
-    const horsesByCustomer = await Horse.find({owner: req.params.id}).exec(function(err, horses) {
-      if (err) return next(err);
-      res.status(200).send(horses);
-    })
-  } catch(err) {
-    return next(err);
-  }
-};
-
-//Update an existing customer
+//Update a customer
 exports.updateCustomer = async (req, res, next) => {
   try {
-    const response = await Customer.findByIdAndUpdate(req.params.id, {$set: req.body.customer}, (err, customer) => {
+    const response = await Customer.findByIdAndUpdate(req.params.customerId, {$set: req.body.customer}, (err, customer) => {
       if (err) return next(err);
       res.status(201).send(customer);
-      console.log(customer);
     });
   } catch(err) {
     return next(err);
   }
 };
 
-//Update the horse of an existing customer
-exports.updateHorseOfCustomer = async (req, res, next) => {
-  try {
-    const response = await Customer.findOneAndUpdate(req.params.id, (err, horse) => {
-      if (err) return next(err);
-      res.status(201).send(customer);
-      console.log(customer);
-    });
-  } catch(err) {
-    return next(err);
-  }
-};
-
-//Delete a Customer
+//Delete a customer
 exports.deleteCustomer = async (req,res,next) => {
   try {
-    Customer.findByIdAndDelete(req.params.id, (err, customer) => {
+    Customer.findByIdAndDelete(req.params.customerId, (err, customer) => {
       if (err) return next(err);
       res.status(200).send(`The customer was succesfully deleted`);
     });
@@ -113,9 +65,8 @@ exports.deleteCustomer = async (req,res,next) => {
 //Add a contact to a customer
 exports.addContact = async (req,res,next) => {
   try {
-    console.log(req.body);
     var opts = { runValidators: true };
-    Customer.findByIdAndUpdate(req.params.id, {$push: {contacts: req.body.contact}}, opts, (err, customer) => {
+    Customer.findByIdAndUpdate(req.params.customerId, {$push: {contacts: req.body.contact}}, opts, (err, customer) => {
       if (err) return next(err);
       res.status(200).send(`The contact was succesfully added`);
     });
@@ -127,8 +78,7 @@ exports.addContact = async (req,res,next) => {
 //Update the contact of a customer
 exports.updateContact = async (req,res,next) => {
   try {
-    console.log(req.body);
-    await Customer.findById(req.params.id, (err, customer) => {
+    await Customer.findById(req.params.customerId, (err, customer) => {
       const contact = customer.contacts.id(req.params.contactId);
       contact.set(req.body.contact);
       customer.save().then(function(savedPost) {
@@ -141,10 +91,11 @@ exports.updateContact = async (req,res,next) => {
     return next(err);
   }
 }
-//Remove a contact from a customer
+
+//Delete a contact from a customer
 exports.deleteContact = async (req,res,next) => {
   try {
-    Customer.findByIdAndUpdate(req.params.id, {$pull: {contacts: {_id: req.params.contactId}}}, (err, customer) => {
+    Customer.findByIdAndUpdate(req.params.customerId, {$pull: {contacts: {_id: req.params.contactId}}}, (err, customer) => {
       res.status(200).send(`The contact was succesfully removed`);
     });
   } catch(err) {
@@ -152,8 +103,54 @@ exports.deleteContact = async (req,res,next) => {
   }
 }
 
-//Get countries
-exports.getCountries = (req, res, next) => {
-  const countries = Customer.schema.path('country').enumValues;
-  res.status(201).send(countries);
+//Search for a customer
+exports.getCustomerSearch = async (req, res, next) => {
+  try {
+    const respons = await Customer.fuzzySearch(req.params.searchValue;
+    res.status(201).send(respons);
+  } catch(err) {
+    return next(err);
+  }
 };
+
+//Get the horses of a customer
+exports.getHorsesOfCustomer = async (req,res,next) => {
+  try {
+    const horsesByCustomer = await Horse.find({owner: req.params.customerId}).exec(function(err, horses) {
+      if (err) return next(err);
+      res.status(200).send(horses);
+    })
+  } catch(err) {
+    return next(err);
+  }
+};
+
+//Get customer count
+// exports.getCustomerCount = async (req, res, next) => {
+//   try {
+//     await Customer.countDocuments({}, (err, count) => {
+//       if (err) res.status(404).send();
+//       res.status(201).json(count);
+//     });
+//   } catch(err) {
+//     return next(err);
+//   }
+// };
+
+//Update the horse of an existing customer
+// exports.updateHorseOfCustomer = async (req, res, next) => {
+//   try {
+//     const response = await Customer.findOneAndUpdate(req.params.customerId, (err, horse) => {
+//       if (err) return next(err);
+//       res.status(201).send(customer);
+//     });
+//   } catch(err) {
+//     return next(err);
+//   }
+// };
+
+//Get countries
+// exports.getCountries = (req, res, next) => {
+//   const countries = Customer.schema.path('country').enumValues;
+//   res.status(201).send(countries);
+// };
--- routes/api/customer.js
+++ routes/api/customer.js
@@ -4,14 +4,17 @@ const customerController = require('../../controllers/customer.js');
 
 router.post('/', customerController.createCustomer);
 router.get('/', customerController.getAllCustomers);
-router.get('/search/:id', customerController.getCustomerSearch);
-router.get('/count', customerController.getCustomerCount);
-router.get('/countries', customerController.getCountries);
-router.get('/:id', customerController.getCustomer);
-router.get('/:id/horse', customerController.getHorsesOfCustomer);
-router.put('/:id', customerController.updateCustomer);
-router.delete('/:id', customerController.deleteCustomer);
-router.post('/:id/contact', customerController.addContact);
-router.put('/:id/contact/:contactId', customerController.updateContact);
-router.delete('/:id/contact/:contactId', customerController.deleteContact);
+router.get('/:customerId', customerController.getCustomer);
+router.put('/:customerId', customerController.updateCustomer);
+router.delete('/:customerId', customerController.deleteCustomer);
+
+router.post('/:customerId/contact', customerController.addContact);
+router.put('/:customerId/contact/:contactId', customerController.updateContact);
+router.delete('/:customerId/contact/:contactId', customerController.deleteContact);
+
+router.get('/search/:searchValue', customerController.getCustomerSearch);
+router.get('/:customerId/horse', customerController.getHorsesOfCustomer);
+
+// router.get('/count', customerController.getCustomerCount);
+// router.get('/countries', customerController.getCountries);
 module.exports = router;
