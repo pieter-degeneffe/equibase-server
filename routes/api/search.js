eea2cbb74da26af74f7ee3f6538f74ffbf4a6f59
Arne Staf Herbots <arne1herbots@gmail.com>
2020-09-03 09:16:38 +0200

    add swagger for search api

--- routes/api/search.js
+++ routes/api/search.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const searchController = require('../../controllers/search.js');
-
+/**
+ * @swagger
+ * /search/product/{searchValue}:
+ *    get:
+ *      summary: Search for a product
+ *      tags: [Search]
+ *      parameters:
+ *        - in: path
+ *          name: searchValue
+ *          schema:
+ *            type: string
+ *          required: true
+ *      responses:
+ *        "200":
+ *          description: An array of Products
+ */
 router.get('/product/:searchValue', searchController.searchProduct);
+/**
+ * @swagger
+ * /search/{searchValue}:
+ *    get:
+ *      summary: Search for customers and/or horses
+ *      tags: [Search]
+ *      parameters:
+ *        - in: path
+ *          name: searchValue
+ *          schema:
+ *            type: string
+ *          required: true
+ *      responses:
+ *        "200":
+ *          description: Search result array
+ */
 router.get('/:searchValue', searchController.getSearch);
 
 module.exports = router;
